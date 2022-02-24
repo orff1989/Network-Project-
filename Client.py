061f7d355552b65ad2d33ea71d6ad063a7f03101
orff1989 <43110158+orff1989@users.noreply.github.com>
2022-02-24 19:05:52 +0200

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,76 @@
+import socket
+from threading import Thread
+
+# this method getting the first command
+import receiver
+
+
+def geting_info():
+    global str, name, command
+
+    str = input().strip()
+    try:
+        command = str.split()[0]
+        name = str.split()[1]
+
+    except Exception as e:
+        print(e)
+def sendFile():
+
+
+    while not str or command!="connect" or name==None:
+            str = input().strip()
+            try:
+                command = str.split()[0]
+                name = str.split()[1]
+            except Exception as e:
+                print(e)
+
+geting_info()
+
+#defining the server socket
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "127.0.0.1"
+port =55010
+
+#conecting to the server
+soc.connect((host,port))
+
+#adding the new user
+c = f"addC "+ name
+soc.send(c.encode())
+
+def messagesListener():
+    while True:
+        msg = soc.recv(1024).decode()
+        print(msg)
+
+# creating new thread for each client messages
+th1 = Thread(target=messagesListener)
+
+
+# make the thread run until the main thread die
+th1.daemon = True
+
+
+th1.start()
+
+
+
+while True:
+    # getting the message
+    msg = input()
+    soc.send(msg.encode())
+
+    if msg.split()[0]=="download":
+        th2 = Thread(target=receiver.recvFile("file.txt",host))
+        th2.daemon = True
+        th2.start()
+
+# close the socket
+soc.close()
+
+
+
+
+
---
+++ sender.py
@@ -0,0 +1,102 @@
+import socket
+
+#this method prevent calculate ord of int instead of char
+def ordHelper(n):
+    if type(n)==int:
+        return n
+    else:
+        return ord(n)
+
+# this method calculate the checksum of a message
+def checksumCalculator(msg):
+    index = len(msg)
+    if (index & 1):
+        index -= 1
+        sum = ordHelper(msg[index])
+    else:
+        sum = 0
+
+    # calculating the checksum
+    while index > 0:
+        index -= 2
+        sum += (ordHelper(msg[index + 1]) << 8) + ordHelper(msg[index])
+
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+
+    ans = (~ sum) & 0xffff
+    ans = ans >> 8 | ((ans & 0xff) << 8)
+
+    return chr(int(ans / 256)) + chr(ans % 256)
+
+
+buffSize = 100
+
+def theSender(nameOfFile, ip):
+
+    PortSend = 55006
+    PortRecv = 55005
+
+    recvT = (ip, PortRecv)
+    destT = (ip, PortSend)
+
+    # reading the file
+    with open(nameOfFile) as f:
+        data = f.read()
+
+    # adding ~ to the end of the data
+    data = data + " ~"
+
+    # creating the sender and receiver socket
+    socketSender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socketReceiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # binding the socket
+    socketReceiver.bind(recvT)
+    
+    # setting timeout
+    socketReceiver.settimeout(1)
+
+    pos = 0
+    sequence = 0
+
+    while pos < len(data):
+
+        # putting the data to send in the buffer
+        if pos + buffSize > len(data):
+            buff = data[pos:]
+        else:
+            buff = data[pos:pos + buffSize]
+        pos += buffSize
+
+        gotAck = False
+        while not gotAck:
+            m= str(checksumCalculator(buff)) + str(sequence) + str(buff)
+            socketSender.sendto(m.encode(), destT)
+
+            try:
+                # receiving the data from the sender
+                theMsg, address = socketReceiver.recvfrom(4096)
+                theMsg=theMsg.decode()
+
+            except socket.timeout:
+                print("Timeout")
+            else:
+
+                checksum = theMsg[:2]
+                ack_seq = theMsg[5]
+                theMsg=theMsg[2:]
+
+                print(theMsg)
+
+                # checking if the checksum is equal to what we got
+                if checksumCalculator(theMsg) == checksum and ack_seq == str(sequence):
+                    gotAck = True
+
+        # changing the predicted sequence to the other one: 1->0, 0->1
+        sequence = (sequence +1)%2
+
+
+
+if __name__ == '__main__':
+    theSender('file.txt','127.0.0.1')
