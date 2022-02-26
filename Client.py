90ad2c0253c25ade2753069448a3332cc27f9b1c
orff1989 <orff1989@walla.com>
2022-02-26 17:06:15 +0200

    v7

--- Client.py
+++ Client.py
@@ -1,36 +1,140 @@
 import socket
 from threading import Thread
+import os
 
-# this method getting the first command
-import receiver
+######################################### receiver ########################################################
 
+#this method prevent calculate ord of int instead of char
+def ordHelper(n):
+    if type(n)==int:
+        return n
+    else:
+        return ord(n)
+
+# this method calculate the checksum of a message
+def checksumCalculator(msgg):
+    index = len(msgg)
+    if (index & 1):
+        index -= 1
+        sum = ordHelper(msgg[index])
+    else:
+        sum = 0
+
+    # calculating the checksum
+    while index > 0:
+        index -= 2
+        sum += (ordHelper(msgg[index + 1]) << 8) + ordHelper(msgg[index])
+
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+
+    ans = (~ sum) & 0xffff
+    ans = ans >> 8 | ((ans & 0xff) << 8)
+
+    return chr(int(ans / 256)) + chr(ans % 256)
+
+# this method sends message to the dest
+def send(theSocket,theMessage, theDest):
+    checksum = checksumCalculator(theMessage)
+    theData = str(checksum) + str(theMessage)
+    theSocket.sendto(theData.encode(), theDest)
+
+# this method gets the file that was sent
+def recvFile(fileName, ip):
+
+    PortSend = 55005
+    PortRecv = 55006
+
+    recvT = (ip, PortRecv)
+    destT = (ip, PortSend)
+
+    # creating the sender and receiver socket
+    socketSender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socketReceiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # binding the socket
+    socketReceiver.bind(recvT)
+
+    predictedSeq = 0
+    print("receiving...")
+    done=False
+    size=None
+    while True:
+        # receiving the data from the sender
+        theMsg, addr = socketReceiver.recvfrom(4096)
+        theMsg= theMsg.decode()
+
+        # getting the checksum
+        checksum = theMsg[:2]
+        sequence = theMsg[2]
+
+        theMsg = theMsg[3:]
+
+        # if received all the data it will be true
+        if theMsg.split()[-1] == "~":
+            done=True
+
+        # checking if the checksum is equal to what we got
+        if checksumCalculator(theMsg) == checksum:
+
+            send(socketSender,"ACK" + sequence, destT)
+
+            # checking if the the sequence is the predicted one
+            if sequence == str(predictedSeq):
+
+                if size==None:
+                    size = int(theMsg.split()[0])
+                    theMsg = theMsg.replace(str(size) + " ", "", 1)
+
+                theMsg = theMsg.replace(" ~", "")
+
+                # open new file and copy the data to it
+                with open(fileName, 'a') as f:
+                    f.write(theMsg)
+
+                print(theMsg)
+
+                currSize = os.path.getsize(fileName)
+                prec = 100 * currSize / size
+
+                print("You downloaded " + str("{:.2f}".format(prec)) + "% out of file. Last byte is: " + str(
+                    currSize) + ".")
+
+                # changing the predicted sequence to the other one: 1->0, 0->1
+                predictedSeq = (predictedSeq +1)%2
+
+                # true if got all the data that was sent
+                if done:
+                    break
+
+        else:
+            NAK = str((predictedSeq+1)%2)
+            send("ACK" + NAK, destT)
+
+
+################################################# Client #####################################################
 
 def geting_info():
-    global str, name, command
+    global info, name, command
 
-    str = input().strip()
+    info = input().strip()
     try:
-        command = str.split()[0]
-        name = str.split()[1]
+        command = info.split()[0]
+        name = info.split()[1]
 
     except Exception as e:
         print(e)
 
-# def sendFile():
-#     while not str or command!="connect" or name==None:
-#             str = input().strip()
-#             try:
-#                 command = str.split()[0]
-#                 name = str.split()[1]
-#             except Exception as e:
-#                 print(e)
 
 geting_info()
 
+hostName = socket.gethostname()
+host = socket.gethostbyname(hostName)
+
+port =55010
+
 #defining the server socket
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "127.0.0.1"
-port =55010
 
 #conecting to the server
 soc.connect((host,port))
@@ -44,10 +148,13 @@ def messagesListener():
         msg = soc.recv(1024).decode()
 
         if msg[:13] == "sending file:":
+            try:
+                th2 = Thread(target=recvFile(msg[14:],host))
+                th2.daemon = True
+                th2.start()
 
-            th2 = Thread(target=receiver.recvFile(msg[14:],host))
-            th2.daemon = True
-            th2.start()
+            except Exception as e:
+                print(e)
 
         else: print(msg)
 
@@ -55,24 +162,18 @@ def messagesListener():
 # creating new thread for each client messages
 th1 = Thread(target=messagesListener)
 
-
 # make the thread run until the main thread die
 th1.daemon = True
-
-
 th1.start()
-
 
 while True:
     # getting the message
-    msg = input()
-    soc.send(msg.encode())
+    try:
+        msg = input()
+        soc.send(msg.encode())
 
-    # if msg.split()[0]=="download":
-    #
-    #     th2 = Thread(target=receiver.recvFile(msg.split()[1],host))
-    #     th2.daemon = True
-    #     th2.start()
+    except Exception as e:
+        print(e)
 
 # close the socket
 soc.close()
--- server.py
+++ server.py
@@ -2,8 +2,118 @@ import socket
 from threading import Thread
 import os
 
-import sender
+############################################# SENDER###############################
 
+# this method prevent calculate ord of int instead of char
+def ordHelper(n):
+    if type(n) == int:
+        return n
+    else:
+        return ord(n)
+
+
+# this method calculate the checksum of a message
+def checksumCalculator(msg):
+    index = len(msg)
+    if (index & 1):
+        index -= 1
+        sum = ordHelper(msg[index])
+    else:
+        sum = 0
+
+    # calculating the checksum
+    while index > 0:
+        index -= 2
+        sum += (ordHelper(msg[index + 1]) << 8) + ordHelper(msg[index])
+
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+
+    ans = (~ sum) & 0xffff
+    ans = ans >> 8 | ((ans & 0xff) << 8)
+
+    return chr(int(ans / 256)) + chr(ans % 256)
+
+
+# this method sends the file to the client
+def theSender(nameOfFile, ip):
+
+    buffSize = 200
+
+    PortSend = 55006
+    PortRecv = 55005
+
+    recvT = (ip, PortRecv)
+    destT = (ip, PortSend)
+
+    # reading the file
+    with open(nameOfFile) as f:
+        data = f.read()
+
+    # getting the size of the file
+    size = os.path.getsize(nameOfFile)
+    data = str(size) + str(" ") + data
+
+    # adding ~ to the end of the data
+    data = data + " ~"
+
+    # creating the sender and receiver socket
+    socketSender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socketReceiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # binding the socket
+    socketReceiver.bind(recvT)
+
+    # setting timeout
+    socketReceiver.settimeout(1)
+
+    pos = 0
+    sequence = 0
+
+    while pos < len(data):
+
+        # putting the data to send in the buffer
+        if pos + buffSize > len(data):
+            buff = data[pos:]
+        else:
+            buff = data[pos:pos + buffSize]
+        pos += buffSize
+
+        gotAck = False
+        while not gotAck:
+            m = str(checksumCalculator(buff)) + str(sequence) + str(buff)
+            socketSender.sendto(m.encode(), destT)
+
+            try:
+                # receiving the data from the sender
+                theMsg, address = socketReceiver.recvfrom(4096)
+                theMsg = theMsg.decode()
+
+            except socket.timeout:
+                print("Timeout")
+
+                if buffSize > 100:
+                    buffSize = buffSize / 2
+            else:
+
+                checksum = theMsg[:2]
+                ack_seq = theMsg[5]
+                theMsg = theMsg[2:]
+
+                print(theMsg)
+
+                # checking if the checksum is equal to what we got
+                if checksumCalculator(theMsg) == checksum and ack_seq == str(sequence):
+                    gotAck = True
+
+                    if buffSize <= 2000:
+                        buffSize = buffSize * 2
+
+        # changing the predicted sequence to the other one: 1->0, 0->1
+        sequence = (sequence + 1) % 2
+
+
+################################ SERVER #####################################
 ip = "0.0.0.0"
 port = 55010
 
@@ -60,84 +170,85 @@ def clientListen(cl, cAddr):
         try:
             msg = cl.recv(1024).decode()
 
-        except Exception as e:
-            print("Error: " + e)
+            print(msg)
 
-        print(msg)
+            source = findBySocet(cl)
 
-        source = findBySocet(cl)
-
-        first_word = msg.split()[0]
-        msg = msg.replace(first_word+" ", "",1)
-
-        # sending the message for every online person
-        if first_word == "set_msg_all":
-            msg= f"{source}: {msg}"
-            broadcast(msg)
-            print("sending broadcast massage: "+ msg)
-
-        # adding a new client to the dict
-        elif first_word == "addC":
-            first_word = msg.split()[0]
-
-            client_sockets[first_word] = cl
-            print("Adding new Client: "+first_word)
-            broadcast("new Client: "+first_word)
-
-        # sending a message to a specific person
-        elif first_word=="set_msg":
             first_word = msg.split()[0]
             msg = msg.replace(first_word+" ", "",1)
-            msg = f"{source}: {msg}"
 
-            sendTo(msg,first_word)
+            # sending the message for every online person
+            if first_word == "set_msg_all":
+                msg= f"{source}: {msg}"
+                broadcast(msg)
+                print("sending broadcast massage: "+ msg)
 
-        elif first_word == "get_users":
-            l = getList(client_sockets)
-            print(l)
-            cl.send(l.encode())
+            # adding a new client to the dict
+            elif first_word == "addC":
+                first_word = msg.split()[0]
 
-            print("returning the online people: "+l)
+                client_sockets[first_word] = cl
+                print("Adding new Client: "+first_word)
+                broadcast("new Client: "+first_word)
 
-        elif first_word == "get_list_file":
-            files = str(os.listdir('.'))
-            print("The files are: "+files)
-            cl.send(files.encode())
+            # sending a message to a specific person
+            elif first_word=="set_msg":
+                first_word = msg.split()[0]
+                msg = msg.replace(first_word+" ", "",1)
+                msg = f"{source}: {msg}"
 
-        elif first_word == "disconnect":
-            n=""
-            for name, soc in client_sockets.items():
-                if cl==soc:
-                    broadcast("disconnected: " + name)
+                sendTo(msg,first_word)
 
-                    soc.close()
-                    client_sockets.pop(name)
-                    print("disconnecting: " + name)
+            elif first_word == "get_users":
+                l = getList(client_sockets)
+                print(l)
+                cl.send(l.encode())
 
+                print("returning the online people: "+l)
 
-        elif first_word== "download":
-            files = os.listdir('.')
+            elif first_word == "get_list_file":
+                files = str(os.listdir('.'))
+                print("The files are: "+files)
+                cl.send(files.encode())
 
-            if msg in files:
-                cl.send(("sending file: "+msg).encode())
-                print("asking to download: "+ msg)
-                sender.theSender(msg,cAddr[0])
-            else:
-                cl.send("There is no such file.".encode())
+            elif first_word == "disconnect":
+                n=""
+                for name, soc in client_sockets.items():
+                    if cl==soc:
+                        broadcast("disconnected: " + name)
 
+                        soc.close()
+                        client_sockets.pop(name)
+                        print("disconnecting: " + name)
+
+            elif first_word== "download":
+                files = os.listdir('.')
+
+                if msg in files:
+                    cl.send(("sending file: "+msg).encode())
+                    print("asking to download: "+ msg)
+                    theSender(msg,cAddr[0])
+                else:
+                    cl.send("There is no such file.".encode())
+
+        except Exception as e:
+            print("Error: " + e)
 while True:
-    #accepting the data that was sent to the server
-    cSoc, cAddress = s.accept()
+    try:
+        # accepting the data that was sent to the server
+        cSoc, cAddress = s.accept()
 
-    cSoc.send("connected".encode())
+        cSoc.send("connected".encode())
 
-    #creating new thread for each client messages
-    th = Thread(target=clientListen, args=(cSoc,cAddress))
+        # creating new thread for each client messages
+        th = Thread(target=clientListen, args=(cSoc,cAddress))
 
-    #make the thread run until the main thread die
-    th.daemon = True
-    # start the thread
-    th.start()
+        # make the thread run until the main thread die
+        th.daemon = True
+        th.start()
+
+    except Exception as e:
+        print(e)
 
 #closing the sockets
 for cs in client_sockets:
